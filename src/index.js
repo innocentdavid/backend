f2551e846c3f3112ba955d2679ebfb1926e5427f
innocentdavid <paulinnocent04@gmail.com>
2022-12-02 08:27:14 +0100

    skdl

--- src/index.js
+++ src/index.js
@@ -33,6 +33,12 @@ const app = express();
 // 	req.headers.origin = req.headers.origin || req.headers.host;
 // 	next();
 // });
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 // app.use(cors(corsOptions));
 app.use(cors({
 	origin: '*'
